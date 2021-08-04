f3a37153ff7e9f6454fe93a5d34fe45e745ae3f3
Benjamin Walkenhorst <krylon@posteo.de>
2021-08-04 12:10:26 +0200

    Adjust cmmon.go to current project

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 23. 07. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-08-04 10:03:52 krylon>
+// Time-stamp: <2021-08-04 11:50:04 krylon>
 
 // Package common contains definitions used throughout the application
 package common
@@ -87,31 +87,14 @@ var LogPath = filepath.Join(BaseDir, fmt.Sprintf("%s.log", strings.ToLower(AppNa
 // DbPath is the filename of the database.
 var DbPath = filepath.Join(BaseDir, fmt.Sprintf("%s.db", strings.ToLower(AppName)))
 
-// CacheDir is the folder where prefetched images are stored.
-var CacheDir = filepath.Join(BaseDir, "cache")
-
-// ArchiveDir is the folder where downloaded/archived pages are stored.
-// var ArchiveDir = filepath.Join("BaseDir", "archive")
-
 // InitApp performs some basic preparations for the application to run.
 // Currently, this means creating the BaseDir folder.
 func InitApp() error {
 	var err error
 
-	CacheDir = filepath.Join(BaseDir, "cache")
-	// ArchiveDir = filepath.Join(BaseDir, "archive")
-
 	if err = os.Mkdir(BaseDir, 0700); err != nil && !os.IsExist(err) {
 		return fmt.Errorf("Error creating BaseDir %s: %s", BaseDir, err.Error())
-	} else if err = os.Mkdir(CacheDir, 0700); err != nil && !os.IsExist(err) {
-		return fmt.Errorf("Error creating CacheDir %s: %s",
-			CacheDir,
-			err.Error())
-	} /* else if err = os.Mkdir(ArchiveDir, 0700); err != nil && !os.IsExist(err) {
-		return fmt.Errorf("Error creating ArchiveDir %s: %s",
-			ArchiveDir,
-			err.Error())
-	} */
+	}
 
 	LogPath = filepath.Join(BaseDir, fmt.Sprintf("%s.log", strings.ToLower(AppName)))
 	DbPath = filepath.Join(BaseDir, fmt.Sprintf("%s.db", strings.ToLower(AppName)))
@@ -134,7 +117,6 @@ func SetBaseDir(path string) error {
 	BaseDir = path
 	LogPath = filepath.Join(BaseDir, fmt.Sprintf("%s.log", strings.ToLower(AppName)))
 	DbPath = filepath.Join(BaseDir, fmt.Sprintf("%s.db", strings.ToLower(AppName)))
-	CacheDir = filepath.Join(BaseDir, "cache")
 
 	var (
 		err error
