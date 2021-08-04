22e5f7272f4077a9be6db76a8cc77d99e4b7d8ea
Benjamin Walkenhorst <krylon@posteo.de>
2021-08-04 10:17:25 +0200

    Add package logdomain

---
+++ logdomain/logdomain.go
@@ -0,0 +1,29 @@
+// /home/krylon/go/src/github.com/blicero/blockbuster/logdomain/id.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 04. 08. 2021 by Benjamin Walkenhorst
+// (c) 2021 Benjamin Walkenhorst
+// Time-stamp: <2021-08-04 10:07:50 krylon>
+
+// Package logdomain provides constants for log sources.
+package logdomain
+
+//go:generate stringer -type=ID
+
+// ID represents a log source
+type ID uint8
+
+// These constants signify the various parts of the application.
+const (
+	Common ID = iota
+	Database
+	Scanner
+)
+
+// AllDomains returns a slice of all the known log sources.
+func AllDomains() []ID {
+	return []ID{
+		Common,
+		Database,
+		Scanner,
+	}
+} // func AllDomains() []ID
