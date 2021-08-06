1526b3876b1a487b47376312341d817d905f62d4
Benjamin Walkenhorst <krylon@posteo.de>
2021-08-06 20:20:57 +0200

    Clean up initialization of TreeViews and TreeModels in GUI

--- ui/ui.go
+++ ui/ui.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 05. 08. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-08-06 15:06:55 krylon>
+// Time-stamp: <2021-08-06 20:19:09 krylon>
 
 // Package ui provides the user interface for the video library.
 package ui
@@ -16,37 +16,16 @@ import (
 	"github.com/blicero/blockbuster/database"
 	"github.com/blicero/blockbuster/logdomain"
 	"github.com/blicero/blockbuster/tree"
-	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
 const refInterval = time.Second // nolint: deadcode
 
-// nolint: deadcode
-const (
-	cfTitle = iota
-	cfSize
-	cfYear
-	cfDirector
-	cfActors
-	cfTags
-)
-
-// nolint: deadcode
-const (
-	cdDirector = iota
-	cdTitle
-	cdYear
-	cdActors
-	cdTags
-)
-
-// nolint: deadcode
-const (
-	ctName = iota
-	ctTitle
-	ctYear
-)
+type tabContent struct {
+	store gtk.ITreeModel
+	view  *gtk.TreeView
+	scr   *gtk.ScrolledWindow
+}
 
 // GUI is the ... well, GUI of the application.
 // Built using Gtk3, which I am still in the process of learning,
@@ -56,23 +35,15 @@ const (
 // So, you've been warned, if you stick around, some interesting times
 // lie ahead!
 type GUI struct {
-	db        *database.Database
-	scan      *tree.Scanner
-	log       *log.Logger
-	lock      sync.RWMutex // nolint: structcheck,unused
-	win       *gtk.Window
-	mainBox   *gtk.Box
-	menubar   *gtk.MenuBar
-	fileScr   *gtk.ScrolledWindow
-	dirScr    *gtk.ScrolledWindow
-	tagScr    *gtk.ScrolledWindow
-	notebook  *gtk.Notebook
-	fileView  *gtk.TreeView
-	dirView   *gtk.TreeView
-	tagView   *gtk.TreeView
-	fileStore *gtk.ListStore
-	dirStore  *gtk.TreeStore
-	tagStore  *gtk.TreeStore
+	db       *database.Database
+	scan     *tree.Scanner
+	log      *log.Logger
+	lock     sync.RWMutex // nolint: structcheck,unused
+	win      *gtk.Window
+	mainBox  *gtk.Box
+	menubar  *gtk.MenuBar
+	notebook *gtk.Notebook
+	tabs     []tabContent
 }
 
 // Create creates a new GUI. You didn't see *that* coming, now, did you?
@@ -113,171 +84,41 @@ func Create() (*GUI, error) {
 		g.log.Printf("[ERROR] Cannot create Notebook: %s\n",
 			err.Error())
 		return nil, err
-	} else if g.fileStore, err = gtk.ListStoreNew(
-		glib.TYPE_STRING, // Title
-		glib.TYPE_STRING, // Size
-		glib.TYPE_INT,    // Year
-		glib.TYPE_STRING, // Director
-		glib.TYPE_STRING, // Actor(s)
-		glib.TYPE_STRING, // Tags
-	); err != nil {
-		g.log.Printf("[ERROR] Cannot create fileStore: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.fileView, err = gtk.TreeViewNewWithModel(g.fileStore); err != nil {
-		g.log.Printf("[ERROR] Cannot create fileView: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.fileScr, err = gtk.ScrolledWindowNew(nil, nil); err != nil {
-		g.log.Printf("[ERROR] Cannot create ScrolledWindow for fileView: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.dirStore, err = gtk.TreeStoreNew(
-		glib.TYPE_STRING, // Director
-		glib.TYPE_STRING, // Title
-		glib.TYPE_INT,    // Year
-		glib.TYPE_STRING, // Actors
-		glib.TYPE_STRING, // Tags
-	); err != nil {
-		g.log.Printf("[ERROR] Cannot create dirStore: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.dirView, err = gtk.TreeViewNewWithModel(g.dirStore); err != nil {
-		g.log.Printf("[ERROR] Cannot create dirView: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.dirScr, err = gtk.ScrolledWindowNew(nil, nil); err != nil {
-		g.log.Printf("[ERROR] Cannot create ScrolledWindow for dirView: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.tagStore, err = gtk.TreeStoreNew(
-		glib.TYPE_STRING, // Name
-		glib.TYPE_STRING, // Title
-		glib.TYPE_INT,    // Year
-	); err != nil {
-		g.log.Printf("[ERROR] Cannot create tagStore: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.tagView, err = gtk.TreeViewNewWithModel(g.tagStore); err != nil {
-		g.log.Printf("[ERROR] Cannot create tagView: %s\n",
-			err.Error())
-		return nil, err
-	} else if g.tagScr, err = gtk.ScrolledWindowNew(nil, nil); err != nil {
-		g.log.Printf("[ERROR] Cannot create ScrolledWindow for tagView: %s\n",
-			err.Error())
-		return nil, err
 	}
 
-	var cTitle, cSize, cYear, cDir, cAct, cTag *gtk.TreeViewColumn
+	g.tabs = make([]tabContent, len(viewList))
 
-	if cTitle, err = createCol("Title", cfTitle); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfTitle: %s\n",
-			err.Error())
-		return nil, err
-	} else if cSize, err = createCol("Size", cfSize); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfSize: %s\n",
-			err.Error())
-		return nil, err
-	} else if cYear, err = createCol("Year", cfYear); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfYear: %s\n",
-			err.Error())
-		return nil, err
-	} else if cDir, err = createCol("Director", cfDirector); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfDirector: %s\n",
-			err.Error())
-		return nil, err
-	} else if cAct, err = createCol("Actor(s)", cfActors); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfActors: %s\n",
-			err.Error())
-		return nil, err
-	} else if cTag, err = createCol("Tags", cfTags); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfTags: %s\n",
-			err.Error())
-		return nil, err
+	for tabIdx, v := range viewList {
+		var (
+			tab tabContent
+			lbl *gtk.Label
+		)
+
+		if tab.store, tab.view, err = v.create(); err != nil {
+			g.log.Printf("[ERROR] Cannot create TreeView %q: %s\n",
+				v.title,
+				err.Error())
+			return nil, err
+		} else if tab.scr, err = gtk.ScrolledWindowNew(nil, nil); err != nil {
+			g.log.Printf("[ERROR] Cannot create ScrolledWindow for %q: %s\n",
+				v.title,
+				err.Error())
+			return nil, err
+		} else if lbl, err = gtk.LabelNew(v.title); err != nil {
+			g.log.Printf("[ERROR] Cannot create title Label for %q: %s\n",
+				v.title,
+				err.Error())
+			return nil, err
+		}
+
+		g.tabs[tabIdx] = tab
+		tab.scr.Add(tab.view)
+		g.notebook.AppendPage(tab.scr, lbl)
+
 	}
-
-	g.fileView.AppendColumn(cTitle)
-	g.fileView.AppendColumn(cSize)
-	g.fileView.AppendColumn(cYear)
-	g.fileView.AppendColumn(cDir)
-	g.fileView.AppendColumn(cAct)
-	g.fileView.AppendColumn(cTag)
-
-	var colDDir, colDTitle, colDYear, colDActors, colDTags *gtk.TreeViewColumn
-
-	if colDDir, err = createCol("Director", cdDirector); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cdDirector: %s\n",
-			err.Error())
-		return nil, err
-	} else if colDTitle, err = createCol("Title", cdTitle); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cdTitle: %s\n",
-			err.Error())
-		return nil, err
-	} else if colDYear, err = createCol("Year", cdYear); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cdYear: %s\n",
-			err.Error())
-		return nil, err
-	} else if colDActors, err = createCol("Actors", cdActors); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cfYear: %s\n",
-			err.Error())
-		return nil, err
-	} else if colDTags, err = createCol("Tags", cdTags); err != nil {
-		g.log.Printf("[ERROR] Cannot create column cdTags: %s\n",
-			err.Error())
-		return nil, err
-	}
-
-	g.dirView.AppendColumn(colDDir)
-	g.dirView.AppendColumn(colDTitle)
-	g.dirView.AppendColumn(colDYear)
-	g.dirView.AppendColumn(colDActors)
-	g.dirView.AppendColumn(colDTags)
-
-	var colTName, colTTitle, colTYear *gtk.TreeViewColumn
-
-	if colTName, err = createCol("Tag", ctName); err != nil {
-		g.log.Printf("[ERROR] Cannot create column ctName: %s\n",
-			err.Error())
-		return nil, err
-	} else if colTTitle, err = createCol("Title", ctTitle); err != nil {
-		g.log.Printf("[ERROR] Cannot create column ctTitle: %s\n",
-			err.Error())
-		return nil, err
-	} else if colTYear, err = createCol("Year", ctYear); err != nil {
-		g.log.Printf("[ERROR] Cannot create column ctYear: %s\n",
-			err.Error())
-		return nil, err
-	}
-
-	g.tagView.AppendColumn(colTName)
-	g.tagView.AppendColumn(colTTitle)
-	g.tagView.AppendColumn(colTYear)
-
-	var fLbl, dLbl, tLbl *gtk.Label
-
-	if fLbl, err = gtk.LabelNew("File"); err != nil {
-		g.log.Printf("[ERROR] Cannot create Label 'Title': %s\n",
-			err.Error())
-		return nil, err
-	} else if dLbl, err = gtk.LabelNew("Director"); err != nil {
-		g.log.Printf("[ERROR] Cannot create Label 'Director': %s\n",
-			err.Error())
-		return nil, err
-	} else if tLbl, err = gtk.LabelNew("Tags"); err != nil {
-		g.log.Printf("[ERROR] Cannot create Label 'Tags': %s\n",
-			err.Error())
-		return nil, err
-	}
-
-	g.notebook.AppendPage(g.fileScr, fLbl)
-	g.notebook.AppendPage(g.dirScr, dLbl)
-	g.notebook.AppendPage(g.tagScr, tLbl)
 
 	g.win.Connect("destroy", gtk.MainQuit)
 
-	g.fileScr.Add(g.fileView)
-	g.dirScr.Add(g.dirView)
-	g.tagScr.Add(g.tagView)
 	g.mainBox.PackStart(g.menubar, false, false, 0)
 	g.mainBox.PackStart(g.notebook, false, false, 0)
 	g.win.Add(g.mainBox)
---
+++ ui/view.go
@@ -0,0 +1,154 @@
+// /home/krylon/go/src/github.com/blicero/blockbuster/ui/view.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 06. 08. 2021 by Benjamin Walkenhorst
+// (c) 2021 Benjamin Walkenhorst
+// Time-stamp: <2021-08-06 19:59:17 krylon>
+
+// The GUI makes generous use of Gtk's TreeView.
+// While TreeView is very versatile and awesome, it can also be very tedious to
+// deal with. In order to make this less tedious, annoying and error-prone, I try
+// to automate away as much of the tedium as possible.
+
+package ui
+
+import (
+	"fmt"
+
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+type storeType uint8
+
+const (
+	storeList storeType = iota
+	storeTree
+)
+
+type column struct {
+	colType glib.Type
+	title   string
+}
+
+type view struct {
+	title   string
+	store   storeType
+	columns []column
+}
+
+func (v *view) typeList() []glib.Type {
+	var res = make([]glib.Type, len(v.columns))
+
+	for i, c := range v.columns {
+		res[i] = c.colType
+	}
+
+	return res
+} // func (v *view) typeList() []glib.Type
+
+func (v *view) create() (gtk.ITreeModel, *gtk.TreeView, error) {
+	var (
+		err   error
+		cols  []glib.Type
+		store gtk.ITreeModel
+		tv    *gtk.TreeView
+	)
+
+	cols = v.typeList()
+	switch v.store {
+	case storeList:
+		if store, err = gtk.ListStoreNew(cols...); err != nil {
+			return nil, nil, err
+		}
+	case storeTree:
+		if store, err = gtk.TreeStoreNew(cols...); err != nil {
+			return nil, nil, err
+		}
+	default:
+		err = fmt.Errorf("invalid Store type %d", v.store)
+		return nil, nil, err
+	}
+
+	if tv, err = gtk.TreeViewNewWithModel(store); err != nil {
+		return nil, nil, err
+	}
+
+	for idx, cSpec := range v.columns {
+		var col *gtk.TreeViewColumn
+		if col, err = createCol(cSpec.title, idx); err != nil {
+			return nil, nil, err
+		}
+		tv.AppendColumn(col)
+	}
+
+	return store, tv, nil
+} // func (v *view) create() (gtk.ITreeModel, gtk.TreeView, error)
+
+var viewList = []view{
+	view{
+		title: "File",
+		store: storeList,
+		columns: []column{
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Title",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Size",
+			},
+			column{
+				colType: glib.TYPE_INT,
+				title:   "Year",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Director",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Actor(s)",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Tags",
+			},
+		},
+	},
+	view{
+		title: "Actor",
+		store: storeTree,
+		columns: []column{
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Name",
+			},
+			column{
+				colType: glib.TYPE_INT,
+				title:   "Born",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Films",
+			},
+		},
+	},
+	view{
+		title: "Tags",
+		store: storeTree,
+		columns: []column{
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Name",
+			},
+			column{
+				colType: glib.TYPE_STRING,
+				title:   "Films",
+			},
+			column{
+				colType: glib.TYPE_INT,
+				title:   "Year",
+			},
+		},
+	},
+}
