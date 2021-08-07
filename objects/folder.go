c30d0670b733a9a254192cefbf484d694594f04a
Benjamin Walkenhorst <krylon@posteo.de>
2021-08-07 21:53:02 +0200

    Add method IsKnown to objects.Folder

--- objects/folder.go
+++ objects/folder.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 07. 08. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-08-07 18:00:26 krylon>
+// Time-stamp: <2021-08-07 21:31:12 krylon>
 
 package objects
 
@@ -14,4 +14,10 @@ type Folder struct {
 	ID       int64
 	Path     string
 	LastScan time.Time
+}
+
+// IsKnown returns true if the Folder's timestamp from the most recent scan
+// is not the epoch, i.e. if the Folder has been scanned before.
+func (f *Folder) IsKnown() bool {
+	return f.LastScan.Unix() != 0
 }
